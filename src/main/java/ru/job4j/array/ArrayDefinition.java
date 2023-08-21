5422fc0cb68431f2d1aeb8c1edcdaec47cce4f7d
Pit <pit777@rambler.ru>
2023-08-21 20:29:19 +0300

    6.0.3. Заполнение массива. [#226300 #241436]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,10 +3,15 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        String[] surnames = new String[100500];
+        String[] names = new String[4];
+        names[0] = "Оля";
+        names[1] = "Коля";
+        names[2] = "Помидоры";
+        names[3] = "Любовь";
         float[] prices = new float[40];
         System.out.println("Число ячеек ages " + ages.length);
-        System.out.println("Число ячеек surnames " + surnames.length);
+        System.out.println("Число ячеек surnames " + names.length);
         System.out.println("Число ячеек prices " + prices.length);
+        System.out.println(names[0] + " + " + names[1] + " + " + names[2] + " = " + names[3]);
     }
 }
