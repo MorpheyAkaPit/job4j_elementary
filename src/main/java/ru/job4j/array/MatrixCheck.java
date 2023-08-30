83aaa720a742343ec78791fe8d91904dbc9e566e
Pit <pit777@rambler.ru>
2023-08-30 22:48:50 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859 #244711]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,26 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWinMyOWN(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X' && (monoVertical(board, i) || monoHorizontal(board, i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (monoVertical(board, i) || monoHorizontal(board, i)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
